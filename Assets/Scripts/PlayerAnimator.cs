60c5b8a52a5969281138480dcf89d0a0c8a4cc26
HappyArroW <stifmeister0700@gmail.com>
2023-07-13 21:26:04 +0200

    Add walk animation and setup cinemachine

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    
+    private Animator _animator;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetBool(IsWalking, player.IsWalking);
+    }
+
+    private void Update()
+    {
+        _animator.SetBool(IsWalking, player.IsWalking);
+    }
+}
